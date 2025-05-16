0b67d70081771c593ee85f677aaac76c4f51bbb0
MaxxxxxxLh <maximilien.lhote@garageisep.com>
2025-05-16 13:32:02 +0200

    .

--- app/Views/pages/annonces.php
+++ app/Views/pages/annonces.php
@@ -1,28 +1,26 @@
-<?php
-$annonces = [
-    [
-        'date' => '2025-04-22',
-        'auteur' => 'Jean Dupont',
-        'animal' => 'Rex (Chien)',
-        'service' => 'Promenade'
-    ],
-    [
-        'date' => '2025-04-21',
-        'auteur' => 'Marie Curie',
-        'animal' => 'Minette (Chat)',
-        'service' => 'Garde'
-    ]
-];
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+    <link rel="stylesheet" href="/css/style.css"> 
+</head>
+<body>
 
-<h2>Liste des annonces</h2>
-
-<?php foreach ($annonces as $annonce): ?>
-    <p>
-        <strong>Service :</strong> <?= $annonce['service'] ?><br>
-        <strong>Date :</strong> <?= $annonce['date'] ?><br>
-        <strong>Auteur :</strong> <?= $annonce['auteur'] ?><br>
-        <strong>Animal :</strong> <?= $annonce['animal'] ?>
-    </p>
-    <hr>
-<?php endforeach; ?>
+    <?php include(__DIR__ . '/../includes/header.php');?>
+    <main>
+        <h2>Liste des annonces</h2>
+        <?php foreach ($annonces as $annonce): ?>
+            <p>
+                <strong>Nom :</strong> <?= $annonce['nom'] ?><br>
+                <strong>Service :</strong> <?= $annonce['service'] ?><br>
+                <strong>Date :</strong> <?= $annonce['date'] ?><br>
+                <strong>Auteur :</strong> <?= $annonce['auteur'] ?><br>
+                <strong>Animal :</strong> <?= $annonce['animal'] ?>
+            </p>
+            <hr>
+        <?php endforeach; ?>
+    </main>
+    <?php include '../includes/footer.php'; ?>
+</body>
+</html>
