c66b0eab04d5fc3df44f05ca52b84f1ca8dc95fd
nath-94 <nathandry94@gmail.com>
2025-05-26 00:42:57 +0200

    correction warning haut de page

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -16,14 +16,16 @@
 
     <main class="container home-page">
         <?php
-            $prenom = isset($_SESSION["user"]["prenom"]) ? htmlspecialchars($_SESSION["user"]["prenom"]) : null;
-            $nom = isset($_SESSION["user"]["nom"]) ? htmlspecialchars($_SESSION["user"]["nom"]) : null;
+            $prenom = htmlspecialchars($_SESSION["user"]["prenom"] ?? '');
+            $nom = htmlspecialchars($_SESSION["user"]["nom"] ?? '');
 
-                echo "<p class='welcome-msg'>Bienvenue, $prenom $nom !</p>";
+            if (!empty($prenom) && !empty($nom)) {
+                echo "<p class='welcome-msg'>Bienvenue, $prenom $nom !</p>";
             } else {
                 echo "<p class='welcome-msg'>Bienvenue sur notre plateforme de gardiennage d’animaux 🐾</p>";
             }
-            ?>
+        ?>
+
             <section class="intro"> 
             <div class="text-box">
                 <h1 class="h1Index">Envie de garder des animaux ou de les promener ?</h1>
